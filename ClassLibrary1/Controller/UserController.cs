0b9392f872bff939acaf93b2475b8a5b9bfa0e6b
Дмитрий Пожарский <skay-deman@mail.ru>
2022-06-26 22:57:25 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Controller/UserController.cs
@@ -0,0 +1,61 @@
+﻿using CodeBlogFitness.BL.Model;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CodeBlogFitness.BL.Controller
+{
+    /// <summary>
+    /// Контроллер пользователя.
+    /// </summary>
+    public class UserController
+    {
+        /// <summary>
+        /// Пользователь приложения.
+        /// </summary>
+        public User User { get; }
+
+        /// <summary>
+        /// Создание нового контроллера пользователя.
+        /// </summary>
+        /// <param name="user"></param>
+        public UserController (string userName, string genderName, DateTime birdthDay, double weight, double height)
+        {
+            var gender = new Gender(genderName);
+
+            User = new User(userName, gender, birdthDay,weight, height);
+
+        }
+
+        public UserController()
+        {
+            var formatter = new BinaryFormatter();
+
+            using (var fs = new FileStream("User.dat", FileMode.OpenOrCreate))
+            {
+                if (formatter.Deserialize(fs) is User user)
+                {
+                    User = user;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Сохранить данные пользователя.
+        /// </summary>
+        public void Save() 
+        {
+            var formatter = new BinaryFormatter();
+            using (var fs = new FileStream("User.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, User);
+            }
+        }
+        /// <summary>
+        /// Получить данные пользователя.
+        /// </summary>
+        /// <returns>Пользовательское приложения.</returns>
+
+        
+    }
+}
---
+++ CodeBlogFitness.CMD/Program.cs
@@ -0,0 +1,32 @@
+﻿using CodeBlogFitness.BL.Controller;
+using System;
+
+
+namespace CodeBlogFitness.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Вас приведстует приложение CodeBlogFitness");
+
+            Console.WriteLine("Введите имя пользователя");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Введите пол");
+            var gender = Console.ReadLine();
+
+            Console.WriteLine("Введите дату рождения");
+            var birthdate = DateTime.Parse (Console.ReadLine());
+
+            Console.WriteLine("Введите вес");
+            var weight = double.Parse (Console.ReadLine());
+
+            Console.WriteLine("Введите рост");
+            var height = double.Parse(Console.ReadLine());
+            var userController = new UserController (name, gender, birthdate, weight, height);
+            userController.Save();
+        }
+    }
+}
